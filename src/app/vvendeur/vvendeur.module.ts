0582eeebbda5b94d69816784da246d36a34a32eb
Serigne koso Dieng <seugnkosso>
2024-09-26 13:05:14 +0000

    creer les module pour Admin Vendeur and public et le core

---
+++ src/app/vvendeur/vvendeur.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VvendeurRoutingModule } from './vvendeur-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    VvendeurRoutingModule
+  ]
+})
+export class VvendeurModule { }
